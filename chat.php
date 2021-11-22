b7bc88b05cefdfa00a6c51d759c2e44bcc704b31
missporu <belomor0688@gmail.com>
2021-11-22 19:27:16 +0500

    1.0.0.4.1

---
+++ chat.php
@@ -0,0 +1,6 @@
+<?php
+$title = 'Чат';
+require_once "system/up.php";
+$user->_Reg();
+
+require_once "system/foot.php";
---
+++ help.php
@@ -0,0 +1,6 @@
+<?php
+$title = 'Поддержка';
+require_once "system/up.php";
+$user->_Reg();
+
+require_once "system/foot.php";
