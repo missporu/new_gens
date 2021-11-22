dc0e94a71482023944fc48fa5ae5e926d275f56a
missporu <belomor0688@gmail.com>
2021-11-22 20:40:42 +0500

    1.0.0.4.1

--- system/class/Filter.php
+++ system/class/Filter.php
@@ -1,19 +1,34 @@
 <?php
 
 class Filter {
-
+    /**
+     * @param $string
+     * @return mixed
+     */
     static public function clearFullSpecialChars($string): mixed {
         return filter_var(value: $string, filter: FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     }
 
+    /**
+     * @param $string
+     * @return string
+     */
     static public function output($string): string {
         return self::clearFullSpecialChars(string: $string);
     }
 
+    /**
+     * @param $string
+     * @return mixed
+     */
     static public function clearString($string): mixed {
         return filter_var(value: $string, filter: FILTER_SANITIZE_STRING);
     }
 
+    /**
+     * @param $string
+     * @return int
+     */
     static public function clearInt($string): int {
         return intval(value: abs(num: filter_var(value: $string, filter: FILTER_SANITIZE_NUMBER_INT)));
     }
